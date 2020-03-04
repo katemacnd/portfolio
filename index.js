f683d636e34fb02735900e5213ea56837e7595c2
Kate Engstrom <katemacnd@gmail.com>
2020-03-04 12:55:06 -0600

    mentor suggested changes - nav bar responsivity

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+/*jshint esversion: 6 */
+
+$(document).ready(function() {
+
 function handleClicks() {
   $('#hamburgerButton').on("click", function() {
     $('.navOptions').toggleClass('hidden');
   });
 }
 
+if ((screen.width<426)) {
+    $(".navOptions").toggleClass('hidden');
+    $('li').click(function() {
+      $(".navOptions").toggleClass('hidden');
+    });
+}
+
 $(handleClicks);
+
+});
